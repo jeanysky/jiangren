80aab1909ea2665a2431a23676b99d8ac353bae4
juanyuan <jeanysky@gmail.com>
2021-09-02 03:04:37 +1000

    scss

---
+++ reactp1/src/App.js
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <div >
+      <header >
+        <p>
+          welcome to react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// import React from "react";
+
+// const component = React.Component;
+
+// class App extends component {
+//   render() {
+//     return (<div >
+//       <header >
+//         <p>
+//           welcome to react
+//         </p>
+//       </header>
+//     </div>
+//     )
+//   }
+// }
+
+// export default App;
